017a95aa4cb275ce0b489e83efdc75219ddb407c
Simon Que <simonque@gmail.com>
2013-03-27 22:43:53 -0700

    Top-level implementation module with AVR interface
    This is the top-level module of a programmable logic design.  It serves as
    a wrapper around the generic ChronoCube module, allowing it to interface
    with an AVR-style memory bus: 16-bit addr muxed with 8-bit data, nWR, nRD,
    ALE signals.

---
+++ main_avr.v
@@ -0,0 +1,128 @@
+// Copyright (C) 2013 Simon Que
+//
+// This file is part of ChronoCube.
+//
+// ChronoCube is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ChronoCube is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with ChronoCube.  If not, see <http://www.gnu.org/licenses/>.
+
+// Top-level implementation module for ChronoCube interfaced with AVR-style bus.
+
+`define RGB_COLOR_DEPTH 18
+
+`define AVR_MPU_ADDR_WIDTH 16
+`define AVR_MPU_DATA_WIDTH 8
+
+`define AVR_MPU_AD_BUS_WIDTH `AVR_MPU_DATA_WIDTH
+`define AVR_MPU_AH_BUS_WIDTH (`AVR_MPU_ADDR_WIDTH - `AVR_MPU_AD_BUS_WIDTH)
+`define AVR_MPU_MAPPING_ADDR 'h8000
+
+`define VRAM_ADDR_WIDTH 16
+`define VRAM_DATA_WIDTH 16
+
+`define MPU_ADDR_WIDTH 16
+`define MPU_DATA_WIDTH 16
+
+module MainAVR(clk, _reset,
+               _mpu_rd, _mpu_wr, mpu_ale, mpu_ah, mpu_ad,
+               _vram_en, _vram_rd, _vram_wr, _vram_be, vram_addr, vram_data,
+               vsync, hsync, rgb);
+
+  input clk;
+  input _reset;
+
+  // To AVR-style memory interface.
+  // TODO: support other interfaces.
+  input _mpu_rd;            // Read enable (active low)
+  input _mpu_wr;            // Write enable (active low)
+  input mpu_ale;            // Address latch enable
+  input [`AVR_MPU_AH_BUS_WIDTH-1:0] mpu_ah;  // Upper address bus
+  inout [`AVR_MPU_AD_BUS_WIDTH-1:0] mpu_ad;  // Multiplexed lower A/D bus
+
+  // To VRAM.
+  output _vram_en;          // Enable access (active low)
+  output _vram_rd;          // Read enable (active low)
+  output _vram_wr;          // Write enable (active low)
+  output [1:0] _vram_be;    // Byte enable (active low)
+  output [`VRAM_ADDR_WIDTH-1:0] vram_addr;   // Address bus
+  inout [`VRAM_DATA_WIDTH-1:0]  vram_data;   // Data bus
+
+  // To VGA connector.
+  output vsync;
+  output hsync;
+  output [`RGB_COLOR_DEPTH-1:0] rgb;
+
+  //////////////////////////////////////////////////////////////////////
+  // Internal MPU bus conversion.
+  // Convert a multiplexed 8-bit data bus to a full 16-bit data bus.
+  // - Latch lower address bits.
+  // - Shift address bits down by 1.
+  // - Determine byte enable.
+  //////////////////////////////////////////////////////////////////////
+  wire [`AVR_MPU_ADDR_WIDTH-1:0] mpu_addr;
+  wire [`AVR_MPU_DATA_WIDTH-1:0] mpu_data_in;
+  wire [`AVR_MPU_DATA_WIDTH-1:0] mpu_data_out;
+
+  // Latch lower address bits.
+  wire [`AVR_MPU_AD_BUS_WIDTH-1:0] mpu_al;
+  CC_DLatch #(`AVR_MPU_AD_BUS_WIDTH)
+    address_latch(.en(mpu_ale),
+                  .d(mpu_ad),
+                  .q(mpu_al));
+  // Shift the address bits down by 1 to convert to a 16-bit bus address.
+  assign mpu_addr = {1'b0, mpu_ah, mpu_al[`AVR_MPU_AD_BUS_WIDTH-1:1]};
+
+  // Handle bidirectional data port.
+  assign mpu_ad = (~_mpu_rd & _mpu_wr & ~mpu_ale) ? mpu_data_out
+                                                  : {`AVR_MPU_DATA_WIDTH{1'bz}};
+  assign mpu_data_in = (~_mpu_wr & _mpu_rd) ? mpu_ad
+                                            : {`AVR_MPU_DATA_WIDTH{1'bz}};
+
+  // Distribute the incoming 8-bit data to both bytes of the 16-bit data port.
+  wire [`MPU_DATA_WIDTH-1:0] mpu_data;
+  assign mpu_data = (~_mpu_wr & _mpu_rd) ? {mpu_data_in, mpu_data_in}
+                                         : {`MPU_DATA_WIDTH {1'bz}};
+
+  // Convert an outgoing 16-bit data bus to an 8-bit data bus by selecting the
+  // high or low byte.
+  assign mpu_data_out =
+      mpu_al[0] ? mpu_data[`MPU_DATA_WIDTH-1:`AVR_MPU_DATA_WIDTH]
+                : mpu_data[`AVR_MPU_DATA_WIDTH-1:0];
+
+  // Determine byte enable based on whether the incoming address is odd or even.
+  wire [1:0] _mpu_be = mpu_al[0] ? 2'b01 : 2'b10;
+
+  // Bus enable is just if either read or write is enabled.
+  wire cc_enable = (~_mpu_rd ^ ~_mpu_wr);
+
+  ChronoCube chronocube(.clk(clk),
+                        ._reset(_reset),
+                        ._mpu_rd(_mpu_rd),
+                        ._mpu_wr(_mpu_wr),
+                        ._mpu_en(~cc_enable),
+                        ._mpu_be(_mpu_be),
+                        .mpu_addr(mpu_addr),
+                        .mpu_data(mpu_data),
+
+                        ._vram_en(_vram_en),
+                        ._vram_rd(_vram_rd),
+                        ._vram_wr(_vram_wr),
+                        ._vram_be(_vram_be),
+                        .vram_addr(vram_addr),
+                        .vram_data(vram_data),
+
+                        .vga_vsync(vsync),
+                        .vga_hsync(hsync),
+                        .vga_rgb(rgb)
+                        );
+
+endmodule
---
+++ main_avr_test.v
@@ -0,0 +1,155 @@
+// Copyright (C) 2013 Simon Que
+//
+// This file is part of ChronoCube.
+//
+// ChronoCube is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ChronoCube is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with ChronoCube.  If not, see <http://www.gnu.org/licenses/>.
+
+
+// Test bench for main module.
+
+module Main_Test;
+
+  parameter ADDR_WIDTH=16;
+  parameter DATA_WIDTH=8;
+
+  reg clk;          // System clock
+  reg reset;        // System reset
+
+  reg rd;           // Read enable
+  reg wr;           // Write enable
+  reg ale;          // Address latch enable.
+  reg [ADDR_WIDTH-1:0] addr;      // Address bus
+  reg [DATA_WIDTH-1:0] data_in;   // Data in bus
+
+  reg data_valid;                 // Indicates data-in is valid.
+
+  wire [DATA_WIDTH-1:0] data_out; // Data out bus
+
+  wire [DATA_WIDTH-1:0] ad;
+  Main main(._reset(~reset), .clk(clk),
+            ._mpu_rd(~rd), ._mpu_wr(~wr), .mpu_ale(ale),
+            .mpu_ah(addr[ADDR_WIDTH-1:DATA_WIDTH]), .mpu_ad(ad));
+
+  assign data_out = (rd & ~wr & ~ale) ? ad : 'bx;
+  assign ad = ale ? addr[DATA_WIDTH-1:0] : (data_valid ? data_in : 'bz);
+
+  // Generate clock.
+  always
+    #1 clk = ~clk;
+
+  integer i;
+  reg [4:0] stage = 0;
+
+  initial begin
+    clk = 0;
+
+    reset = 0;
+    rd = 0;
+    wr = 0;
+    addr = 0;
+    data_in = 0;
+    data_valid = 0;
+
+    // Reset
+    #5 stage = 1;
+    #1 reset = 1;
+    #5 reset = 0;
+
+    #1 addr = 'bx;
+
+    // Test some writes
+    #5 stage = 2;
+    #1 write16(0, 'hdead);
+    #1 write16(2, 'hbeef);
+    #1 write16(4, 'hcafe);
+    #1 write16(8, 'hface);
+    #1 write16(16, 'hbead);
+    #1 write16(18, 'hfade);
+    #1 write16(24, 'hdeaf);
+    #1 write16(26, 'hface);
+    #1 write16(28, 'hface);
+    #1 write16(30, 'hface);
+
+    #1 addr = 'bx;
+
+    // Test some reads
+    #5 stage = 3;
+    #1 read_test();
+
+    #1 addr = 'bx;
+
+    // Test some byte writes
+    #5 stage = 4;
+    for (i = 0; i < 15; i = i + 1)
+    begin
+      #1 write8(i * 2, 'h0000);
+      #1 write8(i * 2 + 1, 'hffff);
+    end
+
+    // Test some reads
+    #5 stage = 5;
+    #1 read_test();
+
+  end
+
+
+  // Task to write a byte.
+  task write8;
+    input [ADDR_WIDTH-1:0] addr_arg;
+    input [DATA_WIDTH-1:0] data_arg;
+    begin
+          addr = addr_arg;
+          data_in = data_arg;
+      #2  rd = 0; wr = 0; ale = 1; data_valid = 0;
+      #2  rd = 0; wr = 1; ale = 0; data_valid = 1;
+      #2  rd = 0; wr = 0; ale = 0; data_valid = 1;
+      #2                           data_valid = 0;
+    end
+  endtask
+
+  // Task to write a word.
+  task write16;
+    input [ADDR_WIDTH-1:0] addr_arg;
+    input [DATA_WIDTH*2-1:0] data_arg;
+    begin
+      write8(addr_arg, data_arg[DATA_WIDTH-1:0]);
+      write8(addr_arg + 1, data_arg[DATA_WIDTH*2-1:DATA_WIDTH]);
+    end
+  endtask
+
+  // Task to read a byte.
+  task read8;
+    input [ADDR_WIDTH-1:0] addr_arg;
+    begin
+          addr = addr_arg;
+      #2  rd = 0; wr = 0; ale = 1;
+      #2  rd = 1; wr = 0; ale = 0;
+      #2  rd = 0; wr = 0; ale = 0;
+    end
+  endtask
+
+  // Readback test for the register
+  task read_test;
+    integer i;
+    begin
+      // Test some reads
+      #5 
+      for (i = 0; i < 30; i = i + 1) begin
+        #1 read8(i);
+      end
+    end
+  endtask
+
+endmodule
+
